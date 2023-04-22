d41a9bd88a438fda87e0062e8f94ccd20f91a3a2
Rithvik Kumar <rithvikyadav2002@gmail.com>
2023-04-23 00:44:24 +0530

    Homepage updated

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,8 @@
 import "./HomePage.scss";
 import { useRef, useEffect } from "react";
 import homePageImg from "../../../public/images/homePageImg.jpg";
-import Heading from "../../components/Headings/Heading";
-import pic1 from "../../../public/images/pic1.jpg";
-import pic2 from "../../../public/images/pic2.jpg";
-import pic3 from "../../../public/images/pic3.jpg";
-import pic4 from "../../../public/images/pic4.jpg";
-import pic5 from "../../../public/images/pic5.jpg";
-import pic6 from "../../../public/images/pic6.jpg";
-import pic7 from "../../../public/images/pic7.jpg";
-import pic8 from "../../../public/images/pic8.jpg";
-
-import { Carousel, initTE } from "tw-elements";
-initTE({ Carousel });
-// Initialization for ES Users
-import { Ripple } from "tw-elements";
-
-initTE({ Ripple });
+import "tw-elements";
+import { Carousel, initTE, Ripple } from "tw-elements";
 
 const HomePage = () => {
   document.title = "ALUMNI Cell NIT Patna | Home";
@@ -46,286 +32,64 @@ const HomePage = () => {
         </div>
       </div>
 
-
-
-
-      {/* Carousel starts here */}
-
-     <div className="container-sm px-1">
- <div id="carouselExampleIndicators"
-  className="relative "
-  data-te-carousel-init
-  data-te-carousel-slide>
-  {/* <!--Carousel indicators--> */}
-      
-  <div
-    className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-    data-te-carousel-indicators>
-    <button
-      type="button"
-      data-te-target="#carouselExampleIndicators"
-      data-te-slide-to="0"
-      data-te-carousel-active
-      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-current="true"
-      aria-label="Slide 1"></button>
-    <button
-      type="button"
-      data-te-target="#carouselExampleIndicators"
-      data-te-slide-to="1"
-      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-label="Slide 2"></button>
-    <button
-      type="button"
-      data-te-target="#carouselExampleIndicators"
-      data-te-slide-to="2"
-      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-label="Slide 3"></button>
-  </div>
-
-  {/* <!--Carousel items--> */}
-  <div
-    className="relative w-full  overflow-hidden after:clear-both after:block after:content-['']">
-    {/* <!--First item--> */}
-    <div
-      className="relative  float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-      data-te-carousel-item
-      data-te-carousel-active>
-      <img
-        src={pic2}
-        className="block w-full h-[80vh]"
-        alt="Wild Landscape" />
-    </div>
-    {/* <!--Second item--> */}
-    <div
-      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-      data-te-carousel-item>
-      <img
-        src={pic5}
-        className="block w-full h-[80vh]"
-        alt="Camera" />
-    </div>
-    {/* <!--Third item--> */}
-    <div
-      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-      data-te-carousel-item>
-      <img
-        src={pic4}
-        className="block w-full h-[80vh]"
-        alt="Exotic Fruits" />
-    </div>
-  </div>
-
-  {/* <!--Carousel controls - prev item--> */}
-  <button
-    className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-    type="button"
-    data-te-target="#carouselExampleIndicators"
-    data-te-slide="prev">
-    <span className="inline-block h-8 w-8">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="h-6 w-6">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15.75 19.5L8.25 12l7.5-7.5" />
-      </svg>
-    </span>
-    <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-      >Previous</span>
-  </button>
-  {/* <!--Carousel controls - next item--> */}
-  <button
-    className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-    type="button"
-    data-te-target="#carouselExampleIndicators"
-    data-te-slide="next">
-    <span className="inline-block h-8 w-8">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="h-6 w-6">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-      </svg>
-    </span>
-    <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-      >Next</span>
-  </button>
-</div>
-</div> 
-
-
-
-
-
-
-
-
-
-
-      {/* gallery */}
-      {/* <div>
-        <h1>Gallery</h1>
-        <div class="columns-8 mx-3 mt-4 gap-5 bg-black ...">
-          <div className="columns-1">
-            <div className=" hover:scale-125">
-              <img class="max-w-full h-auto aspect-square ..." src={pic2} />
-            </div>
-            <div className="my-6 hover:scale-125 ">
-              <img class="w-full h-auto aspect-square p-2..." src={pic1} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full  aspect-square p-2..." src={pic3} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full aspect-square p-2..." src={pic8} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full aspect-square py-2..." src={pic5} />
-            </div>
-            <div className="my-6  hover:scale-125">
-              <img class="w-full aspect-square my-2..." src={pic7} />
-            </div>
-            <div className="my-6  hover:scale-125">
-              <img class="max-w-full aspect-square my-2..." src={pic4} />
-            </div>
-            <div className="my-6 max-w-full h-auto flex align-center aspect-square">
-              <button
-                type="button"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-                className="  bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-              >
-                <a
-                  className=" text-lg text-black hover:font-bold"
-                  href="/gallery"
-                >
-                  {" "}
-                  See More{" "}
-                </a>
-              </button>
-            </div>
+      <div className="grid bg-white text-black p-20  gap-6 text-center md:grid-cols-3 lg:gap-12">
+        <div className="mb-12 md:mb-0">
+          <h1 className="mb-4 text-5xl font-semibold text-center md:text-left">
+            From Director's Desk
+          </h1>
+          <h5 className="mb-4 text-2xl font-semibold text-center md:text-left">
+            Prof. P. K. Jain
+          </h5>
+          <ul className="text-center md:text-left">
+            <h6 className="mb-4 font-semibold text-primary dark:text-primary-500">
+              Director of NIT Patna
+            </h6>
+            <h6 className="mb-4 font-semibold text-primary dark:text-primary-500">
+              Institute AI & ML Chair Professor
+            </h6>
+            <h6 className="mb-4 font-semibold text-primary dark:text-primary-500">
+              Dean – Alumni and Corporate
+            </h6>
+            <h6 className="mb-4 font-semibold text-primary dark:text-primary-500">
+              Relations
+            </h6>
+          </ul>
+        </div>
+        <div className="mb-12 md:mb-0">
+          <div className="mb-12 flex justify-center ">
+            <img
+              src="https://raw.githubusercontent.com/Rajnishk4310/HealthXoxo/main/images/PKJ_Desk%20Photo.jpg"
+              className="w-96 rounded-full shadow-lg dark:shadow-black/30"
+            />
           </div>
         </div>
-      </div> */}
-
-
-
-{/* gallery demo */}
-{/* <div>
-        <h1 className="mx-3 text-lg">Gallery</h1>
-        <div class=" columns-4  sm:columns-8 mx-3 mt-4 gap-5 bg-black  ...">
-          <div className="columns-1">
-            <div className=" hover:scale-125">
-              <img class="max-w-full h-auto aspect-square ..." src={pic2} />
-            </div>
-            <div className="my-6 hover:scale-125 ">
-              <img class="w-full h-auto aspect-square p-2..." src={pic1} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full  aspect-square p-2..." src={pic3} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full aspect-square p-2..." src={pic8} />
-            </div>
-            <div className="my-6 hover:scale-125">
-              <img class="w-full aspect-square py-2..." src={pic5} />
-            </div>
-            <div className="my-6  hover:scale-125">
-              <img class="w-full aspect-square my-2..." src={pic7} />
-            </div>
-            <div className="my-6  hover:scale-125">
-              <img class="max-w-full aspect-square my-2..." src={pic4} />
-            </div>
-            <div className=" aspect-square sm:my-6 max-w-full h-auto flex align-center aspect-square">
-              <button
-                type="button"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-                className="  bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-              >
-                <a
-                  className=" text-xs sm:text-lg text-black hover:font-bold"
-                  href="/gallery"
-                >
-                  {" "}
-                  See More{" "}
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
-
-
-
-
-      <div>
-        <h1 className="mx-3 text-lg">Gallery</h1>
-        <div class="  columns-4  sm:columns-8 mx-3 mt-4 gap-5 bg-black  ...">
-          <div className="columns-1">
-            <div className="overflow-hidden ">
-              <img class="max-w-full h-auto aspect-square hover:scale-125 duration-1000 p-2  ..." src={pic2} />
-            </div>
-            <div className="my-6 overflow-hidden ">
-              <img class="w-full h-auto aspect-square hover:scale-125 duration-1000 p-2  ..." src={pic1} />
-            </div>
-            <div className="my-6 overflow-hidden">
-              <img class="w-full  aspect-square p-2 hover:scale-125 duration-1000 ..." src={pic3} />
-            </div>
-            <div className="my-6 overflow-hidden">
-              <img class="w-full aspect-square p-2 hover:scale-125 duration-1000 ..." src={pic8} />
-            </div>
-            <div className="my-6 overflow-hidden">
-              <img class="w-full aspect-square p-2 hover:scale-125 duration-1000 ..." src={pic5} />
-            </div>
-            <div className="my-6  overflow-hidden">
-              <img class="w-full aspect-square p-2 hover:scale-125 duration-1000 ..." src={pic7} />
-            </div>
-            <div className="my-6  overflow-hidden">
-              <img class="max-w-full aspect-square p-2 hover:scale-125 duration-1000 ..." src={pic4} />
-            </div>
-            <div className=" aspect-square sm:my-6 p-2 max-w-full h-auto flex align-center  aspect-square">
-              <button
-                type="button"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-                className="  bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-              >
-                <a
-                  className=" text-xs sm:text-lg text-black hover:font-bold"
-                  href="/gallery"
-                >
-                  {" "}
-                  See More{" "}
-                </a>
-              </button>
-            </div>
-          </div>
+        <div className="mb-12 md:mb-0">
+          <p className="mb-4 text-2xl text-center md:text-left">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="inline-block h-7 w-7 pr-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+            </svg>
+            Alumni, corporates and philanthropists have been generously
+            contributing towards the development of NIT Patna. The Institute,
+            through the DEAN ACR’s office, inspires and invites these varied
+            groups to continue supporting the aspirations of NIT Patna - an
+            “Institute of Eminence.” We commit to using our donors’ endowments
+            with integrity towards enhancing the welfare of our students and
+            faculty.
+          </p>
+          <button
+            type="button"
+            className="inline-block rounded bg-sky-950 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          >
+            <a className=" decoration-white" href="https://drive.google.com/file/d/1Z4t3povLq2PqdWXaGeJ2g55ge9PbG_Jy/view?sz=w320">READ MORE</a>
+          </button>
         </div>
       </div>
-
-
-
-
-
-
-
-    </>
+    </div>
   );
 };
 
