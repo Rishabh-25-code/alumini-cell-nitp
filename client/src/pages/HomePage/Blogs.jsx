d9ab055e5bb0be0de336f51cf46748100f2eddfb
SudhansuuRanjan <sudhanshur.ug20.ee@gmail.com>
2023-05-02 19:56:45 +0530

    fixed Testimonial cards

--- client/src/pages/HomePage/Blogs.jsx
+++ client/src/pages/HomePage/Blogs.jsx
@@ -120,7 +120,7 @@ const Blogs = () => {
     ]
 
     return (
-        <div className='pb-10'>
+        <div className='pb-10 mb-20'>
             <h1 className="mb-2.5 mt-10 lg:ml-10 md:ml-10 p-5 lg:text-left md:text-left text-center text-sky-400 lg:text-5xl md:text-4xl text-4xl font-bold leading-tight">
                 Blogs
             </h1>
--- client/src/pages/HomePage/Testimonials.jsx
+++ client/src/pages/HomePage/Testimonials.jsx
@@ -1,32 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaQuoteLeft } from "react-icons/fa";
+import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md"
 
 const Testimonials = () => {
+
+    const [activeIndex, setActiveIndex] = useState(0);
 
     const testimonials = [
         {
             id: 0,
-            name: "Name",
-            image: "images/Alumni3.jpg",
-            batch: "Batch",
-            designation: "Designation",
-            message: "Message"
+            name: "Manas Bihari Verma",
+            image: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Dr._Manas_receiving_Padma_Shri_award_%28cropped%29.jpg",
+            batch: "1987",
+            designation: "Indian Aeronautical Scientist",
+            message: "The Cultural Society organizes a cultural extravaganza under the name Mélange – the melting pot of talents."
         },
         {
             id: 1,
-            name: "Name",
-            image: "images/Alumni1.jpg",
-            batch: "Batch",
-            designation: "Designation",
-            message: "Message"
+            name: "Sudhanshu Ranjan",
+            image: "https://avatars.githubusercontent.com/u/77230416?v=4",
+            batch: "2024",
+            designation: "Frontend Engineer @ Netflix",
+            message: "The institute has been ranked 63 among engineering colleges by the National Institutional Ranking Framework (NIRF) in 2022."
+        },
+        {
+            id: 2,
+            name: "Rishabh Prakash",
+            image: "/images/Alumni1.jpg",
+            batch: "2024",
+            designation: "Software Engineer @ Google",
+            message: "The institute has been ranked 8th among the top 10 engineering colleges in India by the India Today magazine in 2017 and 2018."
         },
         {
             id: 3,
-            name: "Name",
-            image: "images/Alumni1.jpg",
+            name: "PA Sangma",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Ratan_Kumar_Sinha.jpg/480px-Ratan_Kumar_Sinha.jpg",
+            batch: "1989",
+            designation: "Former CM of Meghalaya",
+            message: "The Common Room Society organizes indoor games competitions like chess, carom, table tennis, badminton, and Su-doku championship."
+        },
+        {
+            id: 4,
+            name: "Ratan Kumar Sinha",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Ratan_Kumar_Sinha.jpg/480px-Ratan_Kumar_Sinha.jpg",
             batch: "Batch",
-            designation: "Designation",
-            message: "Message"
+            designation: "Chairman of AEC",
+            message: "The institute has been ranked 8th among the top 10 engineering colleges in India by the India Today magazine in 2017 and 2018."
+        },
+        {
+            id: 5,
+            name: "Nitish Kumar",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Nitish_Kumar_in_February_2007.jpg/359px-Nitish_Kumar_in_February_2007.jpg",
+            batch: "1995",
+            designation: "CM of Bihar",
+            message: "Incubation Center is officially in the list of incubators under the Bihar & Start Policy 2017."
+        },
+        {
+            id: 6,
+            name: "Bindeshwari Dubey",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/B.dubey%283%29.jpg/383px-B.dubey%283%29.jpg",
+            batch: "1980",
+            designation: "Formar CM of Bihar",
+            message: "These hostels now can accommodate all of the students because of the newly constructed Brahmaputra hostel making it a fully residential campus."
         }
     ]
 
@@ -36,10 +71,28 @@ const Testimonials = () => {
                 What our Alumni say about us
             </h1>
 
-            <div className='flex flex-wrap m-auto px-5 items-center justify-evenly mb-10'>
-                {testimonials.slice(0, 3).map((event, id) => (
-                    <TestimonialCard data={event} key={id} />
-                ))}
+            <div className='relative w-full overflow-y-auto overflow-x-hidden py-10'>
+                <div className='flex m-auto px-5 items-center justify-evenly mb-10 gap-10'>
+                    <TestimonialCard data={activeIndex <= 0 ? testimonials.at(-1) : testimonials[activeIndex - 1]} />
+                    <TestimonialCard data={testimonials[activeIndex]} />
+                    <TestimonialCard data={activeIndex == testimonials.length - 1 ? testimonials[0] : testimonials[activeIndex + 1]} />
+                </div>
+                <div className='flex lg:justify-between md:justify-between justify-center lg:gap-0 md:gap-0 gap-10 items-center lg:px-10 md:px-10 px-5 bg-gray-400 absolute lg:top-44 md:top-44 w-full h-0'>
+                    <button onClick={() => {
+                        if (activeIndex === 0) {
+                            setActiveIndex(testimonials.length - 1);
+                        } else {
+                            setActiveIndex(activeIndex - 1);
+                        }
+                    }} className='text-white rounded-full lg:p-4 md:p-4 p-3 bg-sky-600 bg-opacity-80 hover:bg-sky-800 shadow-lg'><MdOutlineKeyboardArrowLeft size={32} /></button>
+                    <button onClick={() => {
+                        if (activeIndex === testimonials.length - 1) {
+                            setActiveIndex(0);
+                        } else {
+                            setActiveIndex(activeIndex + 1);
+                        }
+                    }} className='text-white rounded-full lg:p-4 md:p-4 p-3 bg-sky-600 hover:bg-sky-800 bg-opacity-80 shadow-lg'><MdOutlineKeyboardArrowRight size={32} /></button>
+                </div>
             </div>
         </div>
     )
@@ -47,11 +100,11 @@ const Testimonials = () => {
 
 const TestimonialCard = ({ data }) => {
     return (
-        <div>
-            <div className='flex items-center overflow-hidden w-full rounded-xl border border-gray-700 max-w-[24rem] border-b-8 border-b-sky-500 hover:border-b-sky-700 p-7 hover:border-gray-600 hover:scale-105 transition-all delay-75 ease-in hover:bg-gray-800'>
+        <div className='lg:w-[24rem] md:w-[24rem] w-[100%]'>
+            <div className='flex items-center overflow-hidden lg:w-[24rem] md:w-[24rem] w-[85vw] rounded-xl border border-gray-700  border-b-8 border-b-sky-600 hover:border-b-sky-500 p-7 hover:border-gray-600 hover:scale-105 transition-all delay-75 ease-in hover:bg-gray-800'>
                 <div className='flex flex-col'>
                     <FaQuoteLeft className='text-sky-400' size={41} />
-                    <p className='text-white font-medium mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit illo at fugiat aperiam ipsa numquam quod voluptates laboriosam unde minus!</p>
+                    <p className='text-white font-medium mt-3'>{data.message}</p>
 
                     <div className='mt-6 flex gap-2 items-center'>
                         <div>
@@ -60,9 +113,9 @@ const TestimonialCard = ({ data }) => {
                             </div>
                         </div>
                         <div>
-                            <h1 className='text-white font-bold text-base'>Nitish Kumar <span>
+                            <h1 className='text-white font-bold text-base'>{data.name} <span>
                                 (1998)</span></h1>
-                            <p className='text-gray-400'>CM Of Bihar</p>
+                            <p className='text-gray-400 text-sm'>{data.designation}</p>
                         </div>
                     </div>
                 </div>
