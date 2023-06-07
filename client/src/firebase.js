b40448b8990ed9809b42c85b349f77c3b0da8708
Sakshi <sakshipandey025@gmail.com>
2023-06-07 16:05:17 +0530

    updated notable-alumni portal

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyABRybBtDTaNXbxaU2QfKfxFp98re168lc",
+  authDomain: "my-app-4d84a.firebaseapp.com",
+  projectId: "my-app-4d84a",
+  storageBucket: "my-app-4d84a.appspot.com",
+  messagingSenderId: "266415560238",
+  appId: "1:266415560238:web:91c126d4470b4230b7f1bf"
 };
 
 // Initialize Firebase
