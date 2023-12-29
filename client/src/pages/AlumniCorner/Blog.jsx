477ac688824194313c68d705cc6b170fac1aca8a
Rishabh Prakash <prakashrishabh3@gmail.com>
2023-12-29 23:03:08 +0530

    added alumni blogs

--- client/src/pages/AlumniCorner/Blog.jsx
+++ client/src/pages/AlumniCorner/Blog.jsx
@@ -8,116 +8,63 @@ const Blog = () => {
 
   const blogs = [
     {
-      id: 0,
-      name: "How to use IPFS in your React App",
-      description: "IPFS is a peer-to-peer hypermedia protocol designed to make the web faster, safer, and more open. In this blog, we will learn how to use IPFS in your React App.",
-      image: "https://gateway.pinata.cloud/ipfs/QmP9MeLgTCSu2CwUW4wwEaFcv1VPbfZz2CtVpHNGSM6LYw",
-      link: "https://medium.com/@sudhanshur705/how-to-use-ipfs-in-your-react-app-1b2e2d7c8c3a",
-      clubName: "TESLA Club",
-      tag: ["IPFS", "ReactJs", "Web3"],
-      author: "Yugank Verma",
-      likes: 29,
-      date: "2022-10-15T00:00:00.000Z",
-      comments: [
-        {
-          id: 1,
-          text: "A real-world utility project solving liquidity in the NFT market.",
-        },
-      ]
+        id: 0,
+        name: "An Open Letter To Pre-final Year Students",
+        description: "I am a final year student when I am writing this blog and its 25th of march 2021. I have passed 3rd year around 8 months back and hence the memories of the pleasures as well as adversities faced by student during their pre final year are still alive.",
+        image: "https://miro.medium.com/v2/resize:fit:402/format:webp/0*HiD-gV3g-qO7Vjds",
+        link: "https://medium.com/nerd-for-tech/an-open-letter-to-pre-final-year-students-2ad36d5831e4",
+        clubName: "Alumni Portal",
+        tag: ["FinalYear", "DSA", "Placements"],
+        author: "Vaishnavi Jha",
+        likes: 42,
+        date: "2021-03-25T00:00:00.000Z",
+        comments: [
+            {
+                id: 1,
+                text: "",
+            },
+        ]
     },
 
 
     {
       id: 1,
-      name: "Algorand and the Pioneer’s Blockchain",
-      description: "The article by Harsh Kumar is a series that explores the blockchain technology behind Algorand with the reference of Harry Poter novel.the author introduces Algorand as a decentralized, scalable, and secure blockchain platform and discusses its key features, such as consensus mechanism and transaction speed. The article also covers the early history and development of Algorand and its founding team.",
-      image: "https://source.unsplash.com/JN0SUcTOig0",
-      link: "https://medium.com/@harshkumar62367/algorand-and-the-pioneers-blockchain-the-beginning-of-a-decentralized-journey-part-1-f76679a28cf0",
-      clubName: "TESLA Club",
-      tag: ["Algorand", "Harry Potter",],
-      author: "Harsh Kumar",
+      name: "My First Seminar Based On Internship Representing Internshala and LinkedIn At NIT Patna",
+      description: "On December 9, 2018, I had an opportunity to organize a seminar on Internship at my own college, National Institute Of Technology Patna(NITP).",
+      image: "https://media.licdn.com/dms/image/C5112AQFEOaP_1Noqow/article-cover_image-shrink_600_2000/0/1540223228569?e=1709164800&v=beta&t=6P8k2AVGEOlOsHyCDNymDTWGEsL6Ti1Omts4HMa0-_I",
+      link: "https://www.linkedin.com/pulse/my-first-seminar-based-internship-representing-internshala-jha/",
+      clubName: "Alumni Portal",
+      tag: ["Internshala", "NITPatna",],
+      author: "Jyotish Jha",
       likes: 25,
-      date: "2023-02-24T00:00:00.000Z",
+      date: "2018-10-22T00:00:00.000Z",
       comments: [
-        {
-          id: 1,
-          text: "One of the best and most interesting blogs found on the internet for Alogrand Blockchain. Really liked it😍",
-        },
+          {
+              id: 1,
+              text: "",
+          },
       ]
-    },
+  },
     {
-      id: 2,
-      name: "Avalanche consensus protocol SIMPLIFIED!!!",
-      description: "It is a beginner-friendly introduction to the Avalanche consensus protocol. The author explains the problem of achieving consensus in a distributed network and how Avalanche solves this problem. The article provides a clear and concise overview of the Avalanche consensus protocol and its potential applications.",
-      image: "https://source.unsplash.com/jmURdhtm7Ng",
-      link: "https://medium.com/@nikhilgupta3501/avalanche-consensus-protocol-simplified-8542e9b36ea4",
-      clubName: "TESLA club",
-      tag: ["Avalanche", "decentralized",],
-      author: "Nikhil Gupta",
-      likes: 19,
-      date: "2022-12-05T00:00:00.000Z",
-      comments: [
-        {
-          id: 1,
-          text: "Very concise and easy to understand blog on Avalanche consensus.",
-        },
-      ]
-    },
-    {
-      id: 3,
-      name: "Filecoin Orbit Member",
-      description: "The author shares their experience as a participant in the program and highlights the benefits of being a part of a supportive and collaborative community. The article also provides an overview of Filecoin and its underlying technology, as well as some insights into the potential use cases for the network.",
-      image: "https://source.unsplash.com/JN0SUcTOig0",
-      link: "https://medium.com/@harshkumar62367/my-experience-as-filecoin-orbit-member-ef438ff14fa0",
-      clubName: "TESLA Club",
-      tag: ["Filecon", "community", "tech",],
-      author: "Harsh Kumar",
-      likes: 9,
-      date: "2021-09-06T00:00:00.000Z",
-      comments: [
-        {
-          id: 1,
-          text: "Looks like Filecoin is a great community to work with and really excited to work with them in the near future.",
-        },
-      ]
-    },
-    {
-      id: 4,
-      name: "AlgoChat: The Ultimate Algorand GPT-3 Chatbot",
-      description: "The article discusses the development of a chatbot called AlgoChat that utilizes GPT-3 technology to facilitate communication within the Algorand ecosystem. The author provides a detailed explanation of the chatbot's features, including its ability to answer queries related to Algorand",
-      image: "https://source.unsplash.com/ioyEITUD2G8",
-      link: "https://medium.com/@yverma0720/algochat-the-ultimate-algorand-gpt-3-chatbot-b2169579e77c",
-      clubName: "TESLA Club",
-      tag: ["Algorand", "ChatGPT", "AI",],
-      author: "Yugank Verma",
-      likes: 39,
-      date: "2023-02-08T00:00:00.000Z",
-      comments: [
-        {
-          id: 1,
-          text: "What a utility tool it is!!!",
-        },
-      ]
-    },
-    {
-      id: 5,
-      name: "Introduction to Polkadot",
-      description: "Provides an overview of Polkadot, a next-generation blockchain platform that aims to connect different blockchain networks together. The author explains the key concepts of Polkadot, and discusses the benefits of using Polkadot for building decentralized applications.",
-      image: "https://source.unsplash.com/ioyEITUD2G8",
-      link: "https://medium.com/@yverma0720/introduction-to-polkadot-4ee164744435",
-      clubName: "TESLA Club",
-      tag: ["Polkadot",],
-      author: "Yugank Verma",
-      likes: 12,
-      date: "2022-10-15T00:00:00.000Z",
-      comments: [
-        {
-          id: 1,
-          text: "One of the beginner guide on Polkadot.",
-        },
-      ]
-    },
-  ]
+        id: 2,
+        name: "EXPERT's TALK - How to Start Career in VLSI without Masters",
+        description: "On high Demand of VLSI Aspirants, Experts and Companies. We at VLSI FOR ALL organise World's Biggest VLSI Mega Recruitment Fair to Connect VLSI aspirants to top VLSI companies. We have collaborated with 200+ top VLSI Companies in more than 120+ Countries to meet demand of VLSI Skilled Engineers in Top VLSI Companies.",
+        image: "https://images.unsplash.com/photo-1562408590-e32931084e23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        link: "https://www.youtube.com/watch?v=-pGpIObWRX8",
+        clubName: "Alumni Portal",
+        tag: ["VLSIforAll", "vlsijobs",],
+        author: "Agam Gupta",
+        likes: 19,
+        date: "2023-04-28T00:00:00.000Z",
+        comments: [
+            {
+                id: 1,
+                text: "",
+            },
+        ]
+    }
+]
+
 
   return (
     <div className='pt-36'>
