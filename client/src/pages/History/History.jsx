56164bfbe7dae396691ce3050f0783a6796296de
lipi150 <112479235+lipi150@users.noreply.github.com>
2023-12-31 12:30:51 +0530

    changed history content

--- client/src/pages/History/History.jsx
+++ client/src/pages/History/History.jsx
@@ -6,7 +6,7 @@ import Meta from "../../components/Meta/Meta";
 const History = () => {
     return (
         <>
-        <Meta name="History" />
+            <Meta name="History" />
             <div>
                 <Heading heading="History of NITP"></Heading>
             </div>
@@ -24,59 +24,43 @@ const History = () => {
                 </div>
                 <div className=" sm:w-8/12 flex-col">
                     <div className="rounded-3xl bg-gray-900 p-6 mb-10">
-                         <div className="flex text-3xl text-sky-500">
-                             <h2>History of NITP</h2>
-                         </div>
-                         <div className=" w-52 mb-5 mt-1 h-1 bg-pink-500 flex justify-center items-center"></div>
-                      
-                        <p>
+                        <div className="flex text-3xl text-sky-500">
+                            <h2>History of NITP</h2>
+                        </div>
+                        <div className=" w-52 mb-5 mt-1 h-1 bg-pink-500 flex justify-center items-center"></div>
 
-                        National Institute of Technology Patna (NIT Patna), 
-                        formerly Bihar School of Engineering and Bihar College 
-                        of Engineering, is a public engineering institution located 
-                        in Patna in the Indian state of Bihar. It was renamed as
-                        NIT Patna, by the Government of India on 28 January 2004. 
-                        NIT Patna marked its humble beginning in 1886 with the 
-                        establishment of pleaders survey training school which was 
-                        subsequently promoted to Bihar College of Engineering Patna 
-                        in 1924. This made this institute the 6th Oldest Engineering 
-                        Institute of India. It is an autonomous institute functioning 
-                        directly under Ministry of Education, Government of India.
-                        NIT Patna origin can be traced to 1886 with the establishment
-                        of a survey training school and subsequent renaming it to Bihar 
-                        college of Engineering in 1900. A graduate level curriculum was
-                        introduced in 1924. It was renamed Bihar College of Engineering 
-                        in 1932. In 2004 the government of India upgraded the college 
-                        to National Institute of Technology (NIT) status, as the state 
-                        of Bihar had lost its only Regional Engineering College (REC), 
-                        located at Jamshedpur, when Jharkhand was carved out of Bihar 
-                        in 2000. By 2002, the Indian government decided to upgrade all 
-                        RECs to NITs, with the aim of having at least one NIT per state. 
-                        Bihar College of Engineering was the first institute to be directly
-                        upgraded to NIT status. In 2007, it was granted Institute of 
-                        National Importance status in accordance with the National 
-                        Institutes of Technology Act, 2007. 
+                        <p>
+                            NIT Patna marked its humble beginning in 1886 with the establishment of pleaders survey training school which was subsequently promoted of Bihar College of Engineering Patna in 1924.
+
+                            This made this institute the 6th Oldest Engineering Institute of India.
+
+                            The graduate level curriculum was later elevated to the post graduate level in 1978.
+
+                            The institute is situated on the south bank of holy river Ganges behind Gandhi Ghat, one of the most important and reverential place of Patna. The Gandhi Ghat is associated with the immersion of ashes of father of the Nation Mahatma Gandhi in the river Ganges.
+
+                            The campus has a picturesque river view with historic building presenting a spectacle of architectural delight and natural beauty.
+
                         </p>
                     </div>
 
                     <div className="rounded-3xl bg-gray-900 p-6 mb-10">
-                         <div className="flex text-3xl text-sky-500">
-                             <h2>Campus</h2>
-                         </div>
-                         <div className=" w-28 mb-5 mt-1 h-1 bg-pink-500 flex justify-center items-center"></div>
+                        <div className="flex text-3xl text-sky-500">
+                            <h2>Campus</h2>
+                        </div>
+                        <div className=" w-28 mb-5 mt-1 h-1 bg-pink-500 flex justify-center items-center"></div>
 
                         <p>
-                           NIT Patna functions from a 40 acres (16 ha) campus 
-                           along Ashok Rajpath, geographically which lies on 
-                           the southern bank of the river Ganges exactly opposite
-                           the point of its confluence with river Gandak. The 
-                           campus lies alongside Gandhi Ghat where students usually 
-                           enjoy scenic views of river Ganges. Land for a new campus, 
-                           a 125 acres (51 ha) plot, has been assigned at Sikandarpur 
-                           village in Bihta, around 40 km from Patna.[2] Earlier it 
-                           was assigned at Dumri village in Bihta. Once NIT-Patna 
-                           shifts to its new campus in Bihta, it will run some 
-                           management courses on the present campus at Ashok Rajpath.
+                            NIT Patna functions from a 40 acres (16 ha) campus
+                            along Ashok Rajpath, geographically which lies on
+                            the southern bank of the river Ganges exactly opposite
+                            the point of its confluence with river Gandak. The
+                            campus lies alongside Gandhi Ghat where students usually
+                            enjoy scenic views of river Ganges. Land for a new campus,
+                            a 125 acres (51 ha) plot, has been assigned at Sikandarpur
+                            village in Bihta, around 40 km from Patna.[2] Earlier it
+                            was assigned at Dumri village in Bihta. Once NIT-Patna
+                            shifts to its new campus in Bihta, it will run some
+                            management courses on the present campus at Ashok Rajpath.
                         </p>
                     </div>
                 </div>
