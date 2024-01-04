8c43100531af416a39f57e6495bc55def49cbd54
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-01-05 00:01:29 +0530

    job offer

--- client/src/pages/JobOffers/job.js
+++ client/src/pages/JobOffers/job.js
@@ -7,34 +7,6 @@ const job = [
             sharedBy:"Prabhat Kumar",
             designation:"GM in NHPC Ltd., Pakaldul HE Project in UT of J&K, 1995 Civil Batch of NIT-P (then BCE, Patna)."
           
-        },
-        {
-            id:1,
-            companyLogo:"./images/jobs/nhpc.jpg",
-            jobTitle:"Trainee Engineer",
-            company:"NHPC",
-            sharedBy:"Prabhat Kumar",
-            designation:"GM in NHPC Ltd., Pakaldul HE Project in UT of J&K, 1995 Civil Batch of NIT-P (then BCE, Patna)."
-          
-        }
-        ,
-        {
-            id:1,
-            companyLogo:"./images/jobs/nhpc.jpg",
-            jobTitle:"Trainee Engineer",
-            company:"NHPC",
-            sharedBy:"Prabhat Kumar",
-            designation:"GM in NHPC Ltd., Pakaldul HE Project in UT of J&K, 1995 Civil Batch of NIT-P (then BCE, Patna)."
-          
-        },
-        {
-            id:1,
-            companyLogo:"./images/jobs/nhpc.jpg",
-            jobTitle:"Trainee Engineer",
-            company:"NHPC",
-            sharedBy:"Prabhat Kumar",
-            designation:"GM in NHPC Ltd., Pakaldul HE Project in UT of J&K, 1995 Civil Batch of NIT-P (then BCE, Patna)."
-          
         }
     ];
 export default job
