0ea95f5815edd9aad3e74d55caec84d78f79452c
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-01-11 15:36:55 +0530

    fixed bugs

--- client/src/pages/ProtectedPages/Dashboard.jsx
+++ client/src/pages/ProtectedPages/Dashboard.jsx
@@ -3,9 +3,9 @@ import Meta from '../../components/Meta/Meta'
 
 const Dashboard = () => {
   return (
-    <div className='pt-24 min-h-screen'>
+    <div className='min-h-screen'>
       <Meta title="Dashboard | Alumni NITP" />
-      <h1>Dashboard</h1>
+      <h1 className='text-3xl font-bold text-sky-500 text-center'>Dashboard</h1>
     </div>
   )
 }
