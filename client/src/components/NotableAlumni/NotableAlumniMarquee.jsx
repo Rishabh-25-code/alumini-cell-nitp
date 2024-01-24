73ccdc760616e301e2336827c218eb4b4f3a344a
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-01-24 16:51:26 +0530

    Update notable alumni information

--- client/src/components/NotableAlumni/NotableAlumniMarquee.jsx
+++ client/src/components/NotableAlumni/NotableAlumniMarquee.jsx
@@ -9,7 +9,7 @@ const NotableAlumniMarquee = () => {
         {
             name: "Manas Bihari Verma",
             designation: "Scientist",
-            about: "Manas Bihari Verma was an Indian aeronautical scientist instrumental in the development of the light combat aircraft, Tejas. In 2018, he was conferred the Padma Sri civilian honour by the President of India.",
+            about: "Manas Bihari Verma was an Indian aeronautical scientist instrumental in the development of the light combat aircraft, Tejas. In 2018, he was conferred the Padma Sri civilian honour by the President of India. After his retirement he launched Mobile Science Lab aimed at promoting science education in Bihar.",
             image: "./notable-alumni/manas-bihari-verma.jpg",
             id: 1
         },
@@ -35,13 +35,6 @@ const NotableAlumniMarquee = () => {
             id: 4
         },
         {
-            name: "Bhubaneswar Behera",
-            designation: "Engineer, writer and scholar",
-            about: "Bhubaneswar Behera was an engineer, writer and scholar from the Kalahandi district of Odisha. He was born in a village called Kusurla in Kalahandi district. He was the first person from Kalahandi to become an engineer.",
-            image: "./notable-alumni/bubneshwar-behere.jpg",
-            id: 5
-        },
-        {
             name: "Shri Arun Kumar Singh",
             designation: "Chairman & CEO, ONGC, (B.Tech- ME, NIT Patna)",
             about: "A proud alumnus of NIT , Patna, Shri Arun Kumar Singh completed his Bachelor's degree in Mechanical Engg. He currently serves as the Chairman & CEO at Oil and Natural Gas Corporation Ltd, Prior to this, he held the distinguished role of Chairman & MD at Bharat Petroleum Corporation Limited. His extensive experience and profound knowledge have played a pivotal role in shaping the landscape of the oil and gas industry in India.",
@@ -51,9 +44,16 @@ const NotableAlumniMarquee = () => {
         {
             name: "Usha Singh",
             designation: "HR Director at MOIL LIMITED",
-            about: "Ms. Usha Singh, the In-charge CMD and first Director (HR) at MOIL Limited, boasts over 30 years of experience, including key roles at NMDC and SAIL, and has received global recognition as one of the top 100 Inspirational Women in Mining in 2020. She holds an Engg. degree and an MBA, with numerous awards for her contributions to the industry.",
+            about: "Ms. Usha Singh, our alumna from ECE department, she is the In-charge CMD and first Director (HR) at MOIL Limited, boasts over 30 years of experience, including key roles at NMDC and SAIL, and has received global recognition as one of the top 100 Inspirational Women in Mining in 2020. She holds an Engg. degree and an MBA, with numerous awards for her contributions to the industry.",
             image: "https://media.licdn.com/dms/image/D4E03AQGb1IYHU037Ig/profile-displayphoto-shrink_400_400/0/1669020811402?e=1711584000&v=beta&t=X13oegTbL3_LR9p-DP4TIGzf8E_t_vecOVQEP5FzYjU",
             id: 9
+        },
+        {
+            name: "Bhubaneswar Behera",
+            designation: "Engineer, writer and scholar",
+            about: "Bhubaneswar Behera was an engineer, writer and scholar from the Kalahandi district of Odisha. He was born in a village called Kusurla in Kalahandi district. He was the first person from Kalahandi to become an engineer.",
+            image: "./notable-alumni/bubneshwar-behere.jpg",
+            id: 5
         },
         {
             name: "Ajit Kumar Mehta",
--- client/src/pages/NotableAlumni/AlumniCard.jsx
+++ client/src/pages/NotableAlumni/AlumniCard.jsx
@@ -3,7 +3,7 @@ import { FaStar } from "react-icons/fa";
 
 const AlumniCard = ({ alum }) => {
     return (
-        <div data-aos="fade-in" className='rounded-3xl shadow-lg bg-[#000000] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] hover:bg-[#101010] border border-gray-900'>
+        <div data-aos="fade-in" className='rounded-3xl shadow-lg bg-[#1c1c1c] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:20px_24px] hover:bg-[#101010] border border-gray-900'>
             <div className='relative flex hover:scale-[98%] transition lg:flex-row md:flex-col flex-col lg:items-start items-center justify-start  lg:py-6 py-10 lg:gap-3 md:gap-3 gap-6 px-5 lg:max-w-full max-w-[22rem] m-auto'>
                 <div className='relative lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full flex items-center justify-center lg:min-w-[6rem]'>
                     <img loading='lazy' className=' lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full' src={alum.image} alt="project" />
