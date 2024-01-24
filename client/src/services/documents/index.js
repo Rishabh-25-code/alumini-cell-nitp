e0f81cf299b8bfc653b9dab1a13b915e0e6cc881
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-01-24 14:26:44 +0530

    Add getAlumniProfileWithUserName function to check if username is already taken

--- client/src/services/documents/index.js
+++ client/src/services/documents/index.js
@@ -109,6 +109,18 @@ export const getAlumniProfile = async (COLLECTION_ID, email) => {
     }
 }
 
+export const getAlumniProfileWithUserName = async (COLLECTION_ID, username) => {
+    try {
+        const res = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('username', [username])
+        ]);
+
+        return res.documents.length > 0 ? false : true;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
 export const getPaginatedDocuments = async (COLLECTION_ID, limit = 20, offset = 0) => {
     try {
         const res = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
