ff685426c651bbd14f8d0c9c8f396ce12cad2f68
Rishabh Prakash <prakashrishabh3@gmail.com>
2024-01-26 13:08:35 +0530

    getting events dynamically

--- client/src/pages/Events/EventCard.jsx
+++ client/src/pages/Events/EventCard.jsx
@@ -1,19 +1,26 @@
 import { Link } from 'react-router-dom'
+import { getImageURL } from '../../services/files'
 
 const EventCard = ({ data }) => {
+    
+    //getting date from date and time
+    const eventDate = new Date(data.date);
+    const date = eventDate.toLocaleDateString('en-GB');
+
+
     return (
         <div data-aos="fade-up" className='lg:w-[24rem] md:w-[20rem] max-w-sm w-full  p-4 border-2 bg-black hover:border-gray-800 border-gray-900 rounded-2xl mt-5'>
             <div className='flex items-center lg:h-52 md:h-48 h-44 overflow-hidden w-full rounded-lg border border-gray-900'>
-                <img loading='lazy' className='w-full' src={data.image} alt={data.name} />
+                <img loading='lazy' className='w-full' src={getImageURL(data.image)} alt={data.title} />
             </div>
-            <p className='text-gray-400 pt-2 pl-2'>{data.clubName}</p>
-            <h3 className='text-xl font-semibold pl-2'>{data.name}</h3>
+            <p className='text-gray-400 pt-2 pl-2'>{data.organizer}</p>
+            <h3 className='text-xl font-semibold pl-2'>{data.title}</h3>
             {/* <p className='text-gray-400 pt-1 pl-2'>{data.description.length !== 0 ? data.description.substring(0, 90) + "..." : null}</p> */}
             <div className='bg-gray-800 rounded-lg p-3 flex justify-between mt-2 flex-col'>
                 <div className='flex justify-between'>
                     <div>
                         <p className='text-gray-400 text-sm'>Date</p>
-                        <p className='text-medium'>{data.date}</p>
+                        <p className='text-medium'>{date}</p>
                     </div>
 
                     <div className='text-right'>
