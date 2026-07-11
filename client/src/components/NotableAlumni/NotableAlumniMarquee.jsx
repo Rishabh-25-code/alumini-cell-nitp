a11237dd871d7f74930165504bb965aad6b1a264
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2026-07-11 11:32:35 +0530

    Fix Tailwind 4 CSS loading

--- client/src/components/NotableAlumni/NotableAlumniMarquee.jsx
+++ client/src/components/NotableAlumni/NotableAlumniMarquee.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import { RxArrowRight } from 'react-icons/rx'
 import { Link } from 'react-router-dom'
 import { getDocuments } from '../../services/documents';
-import Marquee from "react-fast-marquee";
+import MarqueeModule from "react-fast-marquee";
 import { useQuery } from '@tanstack/react-query';
 import AlumniCard from "../../pages/NotableAlumni/AlumniCard";
+
+const Marquee = MarqueeModule.default?.default || MarqueeModule.default || MarqueeModule;
 
 const NotableAlumniMarquee = () => {
     const [screenWidth, setScreenWidth] = useState(getScreenWidth());
